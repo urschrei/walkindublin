46e22740dca78f426a298582da4b7118bbf2c684
Stephan Hügel <shugel@tcd.ie>
2020-04-01 13:01:17 +0100

    Add error handlers

--- distance.py
+++ distance.py
@@ -85,6 +85,24 @@ def handle_invalid_usage(error):
     return response
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(403)
+def page_not_found(e):
+    # note that we set the 403 status explicitly
+    return render_template("403.html"), 403
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    # note that we set the 500 status explicitly
+    return render_template("500.html"), 500
+
+
 @app.route("/")
 def index():
     return render_template("index.jinja")
