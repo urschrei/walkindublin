8e21680a9e15e1fd551ecd89a63dc403973e5e72
Stephan Hügel <shugel@tcd.ie>
2020-04-01 17:13:52 +0100

    No need to simplify

--- walk_limits.py
+++ walk_limits.py
@@ -1,6 +1,4 @@
 import osmnx as ox
-from osmnx.simplify import simplify_graph
-
 
 def truncate(graph, centre, distance=2000):
     centre_node = ox.get_nearest_node(graph, centre)
@@ -8,7 +6,7 @@ def truncate(graph, centre, distance=2000):
         graph, centre_node, max_distance=distance, weight="length", retain_all=False
     )
     gdf_truncated = ox.save_load.graph_to_gdfs(
-        simplify_graph(truncated), nodes=False, edges=True, node_geometry=False, fill_edge_geometry=True
+        truncated, nodes=False, edges=True, node_geometry=False, fill_edge_geometry=True
     )
     # we MAY need .to_crs("EPSG:4326")
     return gdf_truncated["geometry"]
